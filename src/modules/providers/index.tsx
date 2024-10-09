b3688e72ad49120362f4c074fc8105b367a356ab
Vũ Trọng Đạt <81927305+datvuhp2002@users.noreply.github.com>
2024-10-09 18:39:24 +0700

    change version nextjs

--- src/modules/providers/index.tsx
+++ src/modules/providers/index.tsx
@@ -2,15 +2,12 @@
 import { AppContextProvider } from "@/lib/context/app-context";
 import { ModalContextProvider } from "@/lib/context/modal-context";
 import { ToastContextProvider } from "@/lib/context/toast-context";
-import { AntdRegistry } from "@ant-design/nextjs-registry";
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <AntdRegistry>
-      <AppContextProvider>
-        <ToastContextProvider>
-          <ModalContextProvider>{children}</ModalContextProvider>
-        </ToastContextProvider>
-      </AppContextProvider>
-    </AntdRegistry>
+    <AppContextProvider>
+      <ToastContextProvider>
+        <ModalContextProvider>{children}</ModalContextProvider>
+      </ToastContextProvider>
+    </AppContextProvider>
   );
 }
